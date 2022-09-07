dfd9f29e3cd1c622760dbf1bffe9548887fe0db7
115-6 <maksimmelnikov67@gmail.com>
2022-09-07 10:31:36 +0300

    init1

--- t_bot.py
+++ t_bot.py
@@ -1,5 +1,7 @@
 import telebot
 import datetime
+
+
 bot = telebot.TeleBot('5564840509:AAEr9ZxfMHmN-WDsepBGJGaQ0T7UPDN6Lrg')
 keybord1 = telebot.types.ReplyKeyboardMarkup(True)
 keybord1.row('hello', 'bye')
@@ -8,11 +10,14 @@ reply_markup =keybord1
 # bot.send_messege(message.chat.id, 'Привет, ты написал мне /start')
 def start_message(message):
     bot.send_message(message.chat.id,"lets gooo", reply_markup=keybord1)
+    bot.send_message(message.chat.id,"lets gooo", reply_markup=keybord1)
+
 @bot.message_handler(content_types=['text'])
 def send_text(messege):
     if messege.text.lower() == 'hello':
         bot.send_message(messege.chat.id, 'Hi sweeties!')
     elif messege.text.lower == 'bye':
         bot.send_message(messege.chat.id, 'See you later!')
+
 
 bot.polling()
