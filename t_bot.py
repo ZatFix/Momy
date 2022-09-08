2f8f430b433c8a705387469972f9e986bfdbdac4
115-6 <maksimmelnikov67@gmail.com>
2022-09-08 10:13:47 +0300

    init

--- t_bot.py
+++ t_bot.py
@@ -1,11 +1,22 @@
 import telebot
 import datetime
+from pyowm import OWM
+from pyowm.utils.config import get_default_config
 
+owm =   OWM('23232775d430e5fe2ac9a9c2cbdb8410')
+language = get_default_config()
+language['language'] = 'ru'
 
 bot = telebot.TeleBot('5564840509:AAEr9ZxfMHmN-WDsepBGJGaQ0T7UPDN6Lrg')
 keybord1 = telebot.types.ReplyKeyboardMarkup(True)
-keybord1.row('hello', 'bye')
+keybord1.row('hello', 'bye', 'date')
 reply_markup =keybord1
+temperature = owm
+
+
+
+
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     bot.send_message(message.chat.id,"lets gooo", reply_markup=keybord1)
@@ -13,10 +24,30 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def send_text(messege):
+    global a
     if messege.text.lower() == 'hello':
         bot.send_message(messege.chat.id, 'Hi sweeties!')
     elif messege.text.lower() == 'bye':
         bot.send_message(messege.chat.id, 'See you later!')
+    elif messege.text.lower() == 'date':
+        bot.send_message(messege.chat.id, datetime.date.today())
+    elif messege.text.lower() == 'weather':
+        bot.send_message(messege.chat.id, 'choose the city')
+        a = True
+    elif a ==True:
+        manager = owm.weather_manager()
+        observation = manager.weather_at_place(messege.text)
+        weather = observation.weather
+        z = weather.detailed_status
+        x = weather.clouds
+        c = weather.temperature('celsius').get('temp')
+        v = weather.temperature('celsius').get('temp_max')
+        b = weather.temperature('celsius').get('temp_min')
+        n = weather.rain
+        m = weather.wind()
+        bot.send_message(messege.chat.id, f"Сейчас {z}\nОблачность составляет {x}%\nТемпература: {c}\nМаксимальная температура: {v}\nМинимальная температура: {b}\nКоличество осадков: {n}\nВетер: {m}")
+        a = False
+
 
 
 bot.polling()
